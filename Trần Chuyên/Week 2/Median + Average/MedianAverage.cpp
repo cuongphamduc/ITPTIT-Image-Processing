97871a335fb6a1903800c08427b0a12ea9100b01
tranchuyen282 <chuyentran282@gmail.com>
2018-09-22 01:15:47 +0700

    Fliter

--- Trần Chuyên/Week 2/Median + Average/MedianAverage.cpp
+++ Trần Chuyên/Week 2/Median + Average/MedianAverage.cpp
@@ -39,13 +39,36 @@ void median_fliter(Mat dst) {
 
 
 int main() {
-	Mat src, dst1, dst2;
-	src = imread("1.jpg");
+	Mat dst1, dst2;
 	dst2 = imread("1.jpg");
 	dst1 = imread("1.jpg");
 	//AverageFilter(dst1);
 	//median_fliter(dst2);
-	
+
+
+	Mat src = imread("1.jpg");
+	Mat blur_img, sobel_img1, sobel_img2, gauss_img, mykernel_img;
+	double I[3][3] = { -1,-1,-1,-1,8,-1,-1,-1,-1 };
+	Mat M(3, 3, CV_64F, I);
+
+	blur(src, blur_img, Size(3, 3));
+	imshow("Blur", blur_img);
+	imwrite("Blur.jpg", blur_img);
+
+	Sobel(src, sobel_img1, src.depth(), 1, 0, 3);
+	Sobel(src, sobel_img2, src.depth(), 0, 2, 3);
+
+	Laplacian(src, gauss_img, src.depth());
+	filter2D(src, mykernel_img, src.depth(), M);
+
+	imshow("Sobel Image dx=1, dy=0 ", sobel_img1);
+	imwrite("sobel_img1.jpg", sobel_img1);
+	imshow("Sobel Image dx=0, dy=2 ", sobel_img2);
+	imwrite("sobel_img2.jpg", sobel_img2);
+	imshow("Laplace Image ", gauss_img);
+	imwrite("gauss_img.jpg", gauss_img);
+	imshow("My Kernel Image ", mykernel_img);
+	imwrite("mykernel_img.jpg", mykernel_img);
 	waitKey(0);
 	return 0;
 }
